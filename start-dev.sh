b2743cda5468271496a90143b63e36bde21c7369
Aleksandr Vinokurov <aleksandr.vinokurov@billing.ru>
2012-11-20 10:50:34 +0400

    Add 'sync' utility for dev process

--- start-dev.sh
+++ start-dev.sh
@@ -2,6 +2,6 @@
 cd `dirname $0`
 SAFE_PWD=`echo $PWD | sed 's#/cygdrive/\(.\)/#\1:/#'`
 NODE_NAME=winmeserl-dev
-cmd="erl -pa $SAFE_PWD/ebin $SAFE_PWD/deps/*/ebin -sname $NODE_NAME -boot start_sasl -config start-dev -s lager -s winmeserl"
+cmd="erl -pa $SAFE_PWD/ebin $SAFE_PWD/deps/*/ebin -sname $NODE_NAME -boot start_sasl -config start-dev -s lager -s winmeserl -s sync go"
 echo $cmd
 exec $cmd
